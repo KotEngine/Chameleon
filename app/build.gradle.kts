55fa09e4ac82a8b8ec5cae472f767faeb367add3
SantianDev <cubexteams@gmail.com>
2026-08-14 01:27:22 +0300

    Update compileSdk and targetSdk to version 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.kotengine.chameleon"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.kotengine.chameleon"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "0.1.0"
     }
